d3e8dc587a01615247f8bb00baa0be34a9f9f0b6
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-10 16:27:57 +0300

    Add files via upload

---
+++ 3. Подсчёт количества слов.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    system("chcp 1251"); // Установить кодировку
+    std::string line;
+    std::cout << "Введите строку:\n";
+    std::getline(std::cin, line);
+    
+    int words = 0;
+    int i;
+    bool in = false;
+ 
+    if (line.length() >= 0 && line.length() <= 100) {
+        for (i = 0; i < line.length(); i++) {
+            if (line[i] != ' ') {
+                if (!in) {
+                    words++;
+                    in = true;
+                }                                
+            }
+            else {
+                in = false;
+            }
+            
+        }
+        
+        std::cout << words; //<< "\n" << whitespace << "\n" << sigil;
+        
+    }
+    else {
+        std::cout << "Ошибка!!";
+    }
+    
+
+}
